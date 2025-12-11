15d108966c85020bc5a35cf2c82dd4a5acf5ff7c
people08 <126069357+people08@users.noreply.github.com>
2025-12-11 22:14:09 +0800

    Change implementation macro to DYNCREATE
    修改后可以运行

--- MFCNotepad/ChildFrm.cpp
+++ MFCNotepad/ChildFrm.cpp
@@ -3,7 +3,7 @@
 #include "ChildFrm.h"
 #include "Resource.h"
 
-IMPLEMENT_DYNAMIC(CChildFrame, CMDIChildWndEx)
+IMPLEMENT_DYNCREATE(CChildFrame, CMDIChildWndEx)
 
 BEGIN_MESSAGE_MAP(CChildFrame, CMDIChildWndEx)
 END_MESSAGE_MAP()
